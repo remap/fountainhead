0e90a91eed69dc469879b5420fee9a2fcfb73938
Zhehao Wang <wangzhehao410305@gmail.com>
2015-02-11 13:30:31 -0800

    main: parse command line arguments

--- main.py
+++ main.py
@@ -21,16 +21,51 @@
 
 from fountain_script import FountainScript
 from html_generator import FountainHTMLGenerator
+from fountain_parser import ParserVersion
 
-def main():
-    fountainScript = FountainScript('html-test/remap-script.txt')
-    #fountainScript = FountainScript('html-test/Big-Fish.fountain.txt')
-    #print(fountainScript._titlePageContents)
+import sys, getopt
+
+def usage():
+    print('main.py -v <parser version tag> -i <inputfile> -c <cssfile: relevant to outputFile path> -o <outputfile>')
+
+def main(argv):
+    parserVersion = ParserVersion.DEFAULT
+    inputFile = 'html-test/remap-script.txt'
+    outputFile = 'html-test/debug.html'
+    cssFile = 'ScriptCSS.css'
     
-    fountainHTML = FountainHTMLGenerator(fountainScript, 'ScriptCSS.css')
+    try:
+        opts, args = getopt.getopt(argv, 'hv:i:c:o:', ['version=', 'ifile=', 'cfile=', 'ofile='])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            sys.exit()
+        elif opt in ('-i', '--ifile'):
+            inputFile = arg
+        elif opt in ('-o', '--ofile'):
+            outputFile = arg
+        elif opt in ('-v', '--version'):
+            parserVersion = arg
+        elif opt in ('-c', '--cfile'):
+            cssFile = arg
+    
+    print('PARSER: Input file is ' + inputFile)
+    print('PARSER: Output file is ' + outputFile)
+    print('PARSER: CSS file is ' + cssFile + ' (relevant to output file path)')
+    
+    fountainScript = FountainScript(inputFile, parserVersion)
+    
+    fountainHTML = FountainHTMLGenerator(fountainScript, cssFile, parserVersion)
     htmlOutput = fountainHTML.generateHtml()
     
-    print(htmlOutput)
+    # write html to file
+    file = open(outputFile, 'w')
+    file.write(htmlOutput)
+    
+    print('SUCCESS: HTML file written to ' + outputFile)
     
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
