a3a101a23419a26a8abaa66834d61aff517b56d9
zhehaowang <wangzhehao410305@gmail.com>
2015-05-13 19:12:02 -0700

    cue-publisher: add adaptation for service url to video url

--- html-test/includes/onload.js
+++ html-test/includes/onload.js
@@ -6,35 +6,6 @@ function onImportError(event) {
   return;
 }
 
-function getYoutubeVideoUrl(serviceUrl, fetchIfNotExist) {
-  var data = new FormData();
-  data.append('url', serviceUrl);
-  data.append('fetchIfNotExist', fetchIfNotExist);
-  
-  var xhr = new XMLHttpRequest();
-  //xhr.open('POST', 'http://archive-dev.remap.ucla.edu/app/query', true);
-  xhr.open('POST', 'http://localhost:5000/services/get-youtube-url', true);
-  
-  xhr.onload = function () {
-    console.log('Got response from server: ' + this.responseText);
-  };
-  xhr.send(data);
-}
-
-function loadYoutubeVideoUrls(serviceUrls) {
-  var data = new FormData();
-  data.append('urls', JSON.stringify(serviceUrls));
-  
-  var xhr = new XMLHttpRequest();
-  //xhr.open('POST', 'http://archive-dev.remap.ucla.edu/app/query', true);
-  xhr.open('POST', 'http://localhost:5000/services/load-youtube-urls', true);
-  
-  xhr.onload = function () {
-    console.log('Got response from server: ' + this.responseText);
-  };
-  xhr.send(data);
-}
-
 window.onload = function() {
   // Pressing t to toggle the visibility of the ugly floating menu
   var floatingMenuDiv = 'floating-menu';
@@ -45,6 +16,7 @@ window.onload = function() {
     }
   });
   
+  /*
   // Tests for loadYoutubeVideoUrls and getYoutubeVideoUrl
   var ytUrls = ['https://www.youtube.com/watch?v=77rHEjXj-b8', 
                 'https://www.youtube.com/watch?v=XJqm2irnYlM', 
@@ -52,11 +24,8 @@ window.onload = function() {
                 'https://www.youtube.com/watch?v=kr7MDoWLQ14', 
                 'https://www.youtube.com/watch?v=2od2GZg7-88'];
   
+  // This preloads the list of service urls, and store their corresponding video urls on server 
   loadYoutubeVideoUrls(ytUrls);
-  
-  for (var i = 0; i < ytUrls.length; i++) {
-    getYoutubeVideoUrl(ytUrls[i], true);
-  }
-  
+  */
   console.log("onload");
 };
--- html-test/includes/youtube-object.js
+++ html-test/includes/youtube-object.js
@@ -16,6 +16,35 @@ YoutubeObject.prototype.youtubeKeyParser = function (url) {
   }
 }
 
+YoutubeObject.prototype.getYoutubeVideoUrl = function (serviceUrl, fetchIfNotExist, callback) {
+  var data = new FormData();
+  data.append('url', serviceUrl);
+  data.append('fetchIfNotExist', fetchIfNotExist);
+  
+  var xhr = new XMLHttpRequest();
+  //xhr.open('POST', 'http://archive-dev.remap.ucla.edu/app/query', true);
+  xhr.open('POST', 'http://localhost:5000/services/get-youtube-url', true);
+  
+  xhr.onload = function () {
+    callback(this);
+  };
+  xhr.send(data);
+}
+
+YoutubeObject.prototype.loadYoutubeVideoUrls = function (serviceUrls, callback) {
+  var data = new FormData();
+  data.append('urls', JSON.stringify(serviceUrls));
+  
+  var xhr = new XMLHttpRequest();
+  //xhr.open('POST', 'http://archive-dev.remap.ucla.edu/app/query', true);
+  xhr.open('POST', 'http://localhost:5000/services/load-youtube-urls', true);
+  
+  xhr.onload = function () {
+    callback(this);
+  };
+  xhr.send(data);
+}
+
 YoutubeObject.prototype.requestYoutubeItem = function(options, nextPageToken, videoLinks, onDone) {
   if (!options.hasOwnProperty('query')) {
     console.log('Request word not specified, returned.');
