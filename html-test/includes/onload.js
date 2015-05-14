cbfeeeaf4bf8ae967587ef40b35cec7d0b221fca
zhehaowang <wangzhehao410305@gmail.com>
2015-05-13 17:42:02 -0700

    onload: add test for youtube service to video url test code

--- html-test/includes/onload.js
+++ html-test/includes/onload.js
@@ -6,6 +6,35 @@ function onImportError(event) {
   return;
 }
 
+function getYoutubeVideoUrl(serviceUrl, fetchIfNotExist) {
+  var data = new FormData();
+  data.append('url', serviceUrl);
+  data.append('fetchIfNotExist', fetchIfNotExist);
+  
+  var xhr = new XMLHttpRequest();
+  //xhr.open('POST', 'http://archive-dev.remap.ucla.edu/app/query', true);
+  xhr.open('POST', 'http://localhost:5000/services/get-youtube-url', true);
+  
+  xhr.onload = function () {
+    console.log('Got response from server: ' + this.responseText);
+  };
+  xhr.send(data);
+}
+
+function loadYoutubeVideoUrls(serviceUrls) {
+  var data = new FormData();
+  data.append('urls', JSON.stringify(serviceUrls));
+  
+  var xhr = new XMLHttpRequest();
+  //xhr.open('POST', 'http://archive-dev.remap.ucla.edu/app/query', true);
+  xhr.open('POST', 'http://localhost:5000/services/load-youtube-urls', true);
+  
+  xhr.onload = function () {
+    console.log('Got response from server: ' + this.responseText);
+  };
+  xhr.send(data);
+}
+
 window.onload = function() {
   // Pressing t to toggle the visibility of the ugly floating menu
   var floatingMenuDiv = 'floating-menu';
@@ -16,6 +45,18 @@ window.onload = function() {
     }
   });
   
+  // Tests for loadYoutubeVideoUrls and getYoutubeVideoUrl
+  var ytUrls = ['https://www.youtube.com/watch?v=77rHEjXj-b8', 
+                'https://www.youtube.com/watch?v=XJqm2irnYlM', 
+                'https://www.youtube.com/watch?v=pY54WU0C3i4', 
+                'https://www.youtube.com/watch?v=kr7MDoWLQ14', 
+                'https://www.youtube.com/watch?v=2od2GZg7-88'];
+  
+  loadYoutubeVideoUrls(ytUrls);
+  
+  for (var i = 0; i < ytUrls.length; i++) {
+    getYoutubeVideoUrl(ytUrls[i], true);
+  }
   
   console.log("onload");
 };
